447e900497afe584f91d5cff3340472d90f05c0f
Arif Islam <arif@dreamfactory.com>
2016-03-11 19:55:46 -0500

    building generic WemoClient

--- src/WemoClient.php
+++ src/WemoClient.php
@@ -16,49 +16,40 @@ class WemoClient
     public function request($controlUrl, $service, $method, $arguments = [])
     {
         $controlUrl = ltrim($controlUrl, '/');
-        $url = 'http://'.$this->ip.'/'.$controlUrl;
-        $action = $service.'#'.$method;
+        $url = 'http://' . $this->ip . '/' . $controlUrl;
+        $action = $service . '#' . $method;
 
         $xmlHeader = '<?xml version="1.0" encoding="utf-8"?>
-                        <s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
-                            <s:Body>
-                                <u:'.$method.' xmlns:u="'.$service.'">';
+                      <s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
+                      <s:Body>
+                      <u:' . $method . ' xmlns:u="' . $service . '">';
 
-        $xmlFooter = '</u:'.$method.'></s:Body></s:Envelope>';
+        $xmlFooter = '</u:' . $method . '></s:Body></s:Envelope>';
 
         $xmlBody = '';
 
-        foreach($arguments as $key => $value){
-            $xmlBody .= '<'.$key.'>'.$value.'</'.$key.'>';
+        foreach ($arguments as $key => $value) {
+            $xmlBody .= '<' . $key . '>' . $value . '</' . $key . '>';
         }
 
-        $xml = $xmlHeader.$xmlBody.$xmlFooter;
+        $xml = $xmlHeader . $xmlBody . $xmlFooter;
 
         $options = [
-            CURLOPT_URL => $url,
-            CURLOPT_PORT => $this->port,
+            CURLOPT_URL            => $url,
+            CURLOPT_POST           => true,
+            CURLOPT_PORT           => $this->port,
+            CURLOPT_POSTFIELDS     => $xml,
             CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_POST => true,
-            CURLOPT_POSTFIELDS => $xml,
-            CURLOPT_VERBOSE => false,
-            CURLOPT_HTTPHEADER => [
+            CURLOPT_VERBOSE        => false,
+            CURLOPT_HTTPHEADER     => [
                 'Content-Type:text/xml',
-                'SOAPACTION:"'.$action.'"'
+                'SOAPACTION:"' . $action . '"'
             ]
         ];
 
         $ch = curl_init();
         curl_setopt_array($ch, $options);
+
         return curl_exec($ch);
     }
 }
-
-$wc = new WemoClient('192.168.1.69');
-$rs = $wc->request(
-    'upnp/control/bridge1',
-    'urn:Belkin:service:bridge:1',
-    'SetDeviceStatus',
-    ['DeviceStatusList' => '&lt;?xml version=&quot;1.0&quot; encoding=&quot;utf-8&quot;?&gt;&lt;DeviceStatusList&gt;&lt;DeviceStatus&gt;&lt;IsGroupAction&gt;NO&lt;/IsGroupAction&gt;&lt;DeviceID
-available=&quot;YES&quot;&gt;B4750E1B95783E51&lt;/DeviceID&gt;&lt;CapabilityID&gt;10006,10008,30008,30009,3000A&lt;/CapabilityID&gt;&lt;CapabilityValue&gt;0,255:0,,,&lt;/CapabilityValue&gt;&lt;LastEventTimeStamp&gt;0&lt;/LastEventTimeStamp&gt;&lt;/DeviceStatus&gt;&lt;/DeviceStatusList&gt;']
-);
-print $rs;
--- src/test.php
+++ src/test.php
@@ -1,20 +1,13 @@
 <?php
+include_once('WemoClient.php');
+use a15lam\PhpWemo\WemoClient;
 
-$client = new SoapClient(null, ['location'=>'http://192.168.1.68:49153/upnp/control/basicevent1', 'uri'=>'http://192.168.1.68:49153']);
-
-$location = "http://192.168.1.68:49153/upnp/control/basicevent1";
-$action = "urn:Belkin:service:basicevent:1#GetBinaryState";
-$xml = <<<XML
-<?xml version="1.0" encoding="utf-8"?>
-<s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
-    <s:Body>
-        <u:GetBinaryState xmlns:u="urn:Belkin:service:basicevent:1">
-            <BinaryState></BinaryState>
-        </u:GetBinaryState>
-    </s:Body>
-</s:Envelope>
-XML;
-
-$response = $client->__doRequest($xml, $location,   $action, 1, false);
-
-print $response;
+$wc = new WemoClient('192.168.1.69');
+$rs = $wc->request(
+    'upnp/control/bridge1',
+    'urn:Belkin:service:bridge:1',
+    'SetDeviceStatus',
+    ['DeviceStatusList' => '&lt;?xml version=&quot;1.0&quot; encoding=&quot;utf-8&quot;?&gt;&lt;DeviceStatusList&gt;&lt;DeviceStatus&gt;&lt;IsGroupAction&gt;NO&lt;/IsGroupAction&gt;&lt;DeviceID
+available=&quot;YES&quot;&gt;B4750E1B95783E51&lt;/DeviceID&gt;&lt;CapabilityID&gt;10006,10008,30008,30009,3000A&lt;/CapabilityID&gt;&lt;CapabilityValue&gt;0,0:0,,,&lt;/CapabilityValue&gt;&lt;LastEventTimeStamp&gt;0&lt;/LastEventTimeStamp&gt;&lt;/DeviceStatus&gt;&lt;/DeviceStatusList&gt;']
+);
+print $rs;
